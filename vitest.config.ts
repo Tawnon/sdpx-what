441ac8230bb8380fa654e4c3f0bc4f77a4182a3e
Tawnon <nattanonnn59@gmail.com>
2026-08-11 10:31:40 +0700

    test: setup pytest and vitest test harness, separate e2e from unit tests

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+﻿import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    environment: 'node',
+    exclude: [
+      '**/node_modules/**',
+      '**/.git/**',
+      '**/e2e/**',
+      '**/*.pw.spec.ts',
+    ],
+  },
+});
